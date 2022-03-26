4e0ce2343a2a435a23a25ce99f38d9eb9180d5b7
darya <dasha_kobyashova@mail.ru>
2022-03-26 22:26:40 +0300

    12312

---
+++ 123/src/123.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : 123.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+
+
+int main() {
+	int var2;
+	//char s1, s2, s3;
+	scanf("%i", &var2);
+	//s1 = 00000;
+	//s2 = var2;
+	//s3 = s1+s2;
+	printf("%5i",var2);
+	return 0;
+}
